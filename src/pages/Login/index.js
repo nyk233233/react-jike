2fc2226af673c3dcf8e534801862274b52676cc2
flyingfish <2125859734@qq.com>
2025-08-21 11:40:07 +0800

    token持久化“

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,4 +1,39 @@
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from "@/assets/logo.png";
 const Login = () => {
-  return <div>this is Login</div>;
+  const onFinish = (values) => {
+    console.log(values);
+  };
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form onFinish={onFinish} validateTrigger="onBlur">
+          <Form.Item
+            name="moblie" //name与后端接口统一
+            rules={[
+              { required: true, message: "请输入手机号" },
+              { pattern: /^1[3-9]\d{9}$/, message: "格式错误" },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "请输入验证码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 export default Login;
